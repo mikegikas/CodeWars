eb9167ed766e9e51ae58f4ddd14961c0d7de0076
Mike Gikas <mikegikas@users.noreply.github.com>
2019-03-04 17:51:08 +0200

    Add files via upload

--- Merging sorted integer arrays (without duplicates)/src/Kata.java
+++ Merging sorted integer arrays (without duplicates)/src/Kata.java
@@ -1,9 +1,8 @@
-import java.util.Arrays;
-import java.util.TreeSet;
+import java.util.stream.IntStream;
 
 public class Kata {
     public static int[] mergeArrays(int[] first, int[] second) {
-
+        return IntStream.concat(IntStream.of(first), IntStream.of(second)).distinct().sorted().toArray();
     }
 }
 
