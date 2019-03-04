28629a3e2256325d064d25292f32bec4fee4e261
Mike Gikas <mikegikas@users.noreply.github.com>
2019-03-05 00:04:57 +0200

    Add files via upload

---
+++ Convert boolean values to strings 'Yes' or 'No'/src/YesOrNo.java
@@ -0,0 +1,11 @@
+class YesOrNo{
+    public static String boolToWord(boolean b) {
+        if (b){
+            return "Yes";
+        }
+        return "No";
+    }
+}
+
+
+//    Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
---
+++ Invert values/src/Kata.java
@@ -0,0 +1,16 @@
+public class Kata {
+    public static int[] invert(int[] array) {
+        for (int i = 0; i < array.length; i++){
+            array[i] = -array[i];
+        }
+        return array;
+    }
+}
+
+
+//    Given a set of numbers, return the additive inverse of each.
+//    Each positive becomes negatives, and the negatives become positives.
+//
+//    invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+//    invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+//    invert([]) == []
