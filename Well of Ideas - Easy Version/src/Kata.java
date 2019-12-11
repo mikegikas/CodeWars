1ac7a79fb1f5b547aa0766f6c2eee20a4bb0a13d
Mike G <gikasmike@gmail.com>
2019-12-11 22:32:33 +0200

    Well of Ideas - Easy Version

---
+++ Well of Ideas - Easy Version/src/Kata.java
@@ -0,0 +1,26 @@
+public class Kata {
+
+    public static String well(String[] x) {
+        int count = 0;
+        for (String word: x) {
+            if (word.equals("good")){
+                count++;
+            }
+        }
+        if (count == 0){
+            return "Fail!";
+        } else if (count == 1 || count == 2){
+            return "Publish!";
+        } else {
+            return "I smell a series!";
+        }
+    }
+}
+
+
+//    For every good kata idea there seem to be quite a few bad ones!
+//
+//    In this kata you need to check the provided array (x) for good ideas 'good'
+//    and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if
+//    there are more than 2 return 'I smell a series!'. If there are no good ideas, as
+//    is often the case, return 'Fail!'.
